0dbec27a26eca50908f590f12dd0fd45152ac779
Tommy Song <tommynsong@gmail.com>
2022-06-08 10:38:32 -0500

    intentionally comment out security controls

--- s3.tf
+++ s3.tf
@@ -12,15 +12,15 @@ resource "aws_s3_bucket" "bucket" {
     git_org              = "tommynsong"
   }
 }
-resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
+# resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
+#   bucket = aws_s3_bucket.bucket.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:kms"
-    }
-  }
-}
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "aws:kms"
+#     }
+#   }
+# }
 resource "aws_s3_bucket_logging" "bucket" {
   bucket = aws_s3_bucket.bucket.id
 
@@ -39,12 +39,12 @@ resource "aws_s3_bucket_public_access_block" "bucket" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-resource "aws_s3_bucket_versioning" "bucket" {
-  bucket = aws_s3_bucket.bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "bucket" {
+#   bucket = aws_s3_bucket.bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 resource "aws_s3_bucket" "logs" {
   bucket        = "${aws_s3_bucket.bucket.id}-logs"
   force_destroy = true
@@ -63,18 +63,18 @@ resource "aws_s3_bucket_acl" "logs" {
   bucket = aws_s3_bucket.logs.id
   acl    = "log-delivery-write"
 }
-resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
-  bucket = aws_s3_bucket.logs.id
+# resource "aws_s3_bucket_server_side_encryption_configuration" "logs" {
+#   bucket = aws_s3_bucket.logs.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:kms"
-    }
-  }
-}
-resource "aws_s3_bucket_versioning" "logs" {
-  bucket = aws_s3_bucket.logs.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "aws:kms"
+#     }
+#   }
+# }
+# resource "aws_s3_bucket_versioning" "logs" {
+#   bucket = aws_s3_bucket.logs.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
